2b479e7f2f585575216ef049c4ae20a2baa1a527
luyishuang <1781990652@qq.com>
2020-03-31 22:22:16 +0800

    entropy unit test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,23 @@
+/*global module :true*/
 module.exports = function (grunt) {
-
-  grunt.registerTask('build', 'bulid task',function(){
-    console.log('build task!');
+  grunt.initConfig({
+      mochacli: {
+          options: {
+              reporter: 'spec',
+              bail:true
+          },
+          all: ['test/*.js']
+      },
+      mocha_istanbul:{
+          coverage:{
+              src:'test'
+          }
+      }
   });
+
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+  grunt.registerTask('default', ['mochacli']);
+  grunt.registerTask('cover', ['mocha_istanbul']);
 };
